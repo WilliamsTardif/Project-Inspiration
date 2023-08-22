e08cb7d0de9bcf21ecbe124b2c5b57bcea6aadc3
Williams Tardif <williams_tardif@outlook.com>
2023-08-21 21:45:01 -0400

    Design quotes page

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("quotesPage.ejs")
+});
+
+app.listen(port, () => {
+    console.log(`Server is runnning on port: ${port}.`);
+});
